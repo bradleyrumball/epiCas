deba3360afe018379cc4e3a32f5deabde32b9758
Ant Nettleship <a.nettleship@epigenesys.co.uk>
2014-04-17 13:04:23 +0100

    fixed require

--- lib/epi_cas.rb
+++ lib/epi_cas.rb
@@ -1,9 +1,10 @@
-require "epi_cas/devise_helper"
-require "epi_cas/settings"
-
-require "epi_cas/user_authentications/ldap_info"
-require "epi_cas/user_authentications/verifier"
-require "epi_cas/user_authentications/whitelist_checker"
+require 'epi_cas/devise_helper'
+require 'epi_cas/ldap_authenticator'
+require 'epi_cas/ldap_info'
+require 'epi_cas/settings'
+require 'epi_cas/ticket_authenticator'
+require 'epi_cas/verifier'
+require 'epi_cas/whitelist_checker'
 
 module EpiCas
   
