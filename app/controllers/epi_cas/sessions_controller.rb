cc4303aee27e8022b32f1266d28186a71bd0f97e
Ant Nettleship <a.nettleship@epigenesys.co.uk>
2014-04-17 17:30:56 +0100

    tidying up

--- app/controllers/epi_cas/sessions_controller.rb
+++ app/controllers/epi_cas/sessions_controller.rb
@@ -1,15 +1,17 @@
-class EpiCas::SessionsController < EpiCasController
+module EpiCas
+  class SessionsController < ::ApplicationController
   
-  def app_logout
-    @application_title = 'Incident Contacts'
-    @cas_title         = EpiCas::Settings.cas_title
-    render layout: false
-  end  
+    def app_logout
+      @application_title = EpiCas::Settings.app_title
+      @cas_title         = EpiCas::Settings.cas_title
+      render layout: false
+    end  
   
-  # Log out from Portal
-  def cas_logout
-    reset_session
-    redirect_to EpiCas::Settings.cas_logout_url
+    # Log out from Portal
+    def cas_logout
+      reset_session
+      redirect_to EpiCas::Settings.cas_logout_url
+    end
+  
   end
-  
 end
--- app/controllers/epi_cas_controller.rb
+++
@@ -1,6 +0,0 @@
-# All EpiCas controllers are inherited from here
-class EpiCasController < ApplicationController
-  
-  # Common code if needed
-  
-end
