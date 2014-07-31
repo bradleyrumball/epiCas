126dc0a9a94f2d5111b7c86220384ffcde9db24c
Shuo Chen <s.chen@epigenesys.co.uk>
2014-07-31 12:13:40 +0100

    can set after logout url

--- app/controllers/epi_cas/sessions_controller.rb
+++ app/controllers/epi_cas/sessions_controller.rb
@@ -4,6 +4,9 @@ module EpiCas
     def app_logout
       @application_title = EpiCas::Settings.app_title
       @cas_title         = EpiCas::Settings.cas_title
+      
+      @after_logout_url  = EpiCas::Settings.key?('after_logout_url') ? EpiCas::Settings.after_logout_url : main_app.root_path
+      
       render layout: false
     end  
   
