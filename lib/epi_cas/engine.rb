d5c2de34d86250ac19aaced60f9a89c40b82bc05
Shuo Chen <s.chen@epigenesys.co.uk>
2014-11-05 16:52:45 +0000

    override devise failure app to remove the unauthenticated message when using CAS

--- lib/epi_cas/engine.rb
+++ lib/epi_cas/engine.rb
@@ -1,5 +1,21 @@
 module EpiCas
   class Engine < Rails::Engine
     isolate_namespace EpiCas
+    
+    config.after_initialize do
+      if EpiCas::Settings['auth_method'].to_s.downcase == 'cas'
+        Devise::FailureApp.class_eval do
+          def redirect
+            store_location!
+            if flash[:timedout] && flash[:alert]
+              flash.keep(:timedout)
+              flash.keep(:alert)
+            end
+            redirect_to redirect_url
+          end
+        end
+      end
+    end
+    
   end
 end
